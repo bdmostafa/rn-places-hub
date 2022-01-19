03d3d6f0e4c7a8944f645dd9071ba85d853db1ec
bdmostafa <mdmostafaalmahmud@gmail.com>
2022-01-19 15:51:32 +0600

    folder structure with navigation setup

---
+++ screens/NewPlaceScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NewPlaceScreen = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ screens/PlacesListScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const PlacesListScreen = () => {
+  return (
+    <View>
+      <Text>PlacesListScreen</Text>
+    </View>
+  );
+};
+
+PlacesListScreen.navigationOptions = {
+    headerTitle: "All Places List"
+}
