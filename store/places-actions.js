803a596d4357b349d951aed18b25cbc53b08c1e3
bdmostafa <mdmostafaalmahmud@gmail.com>
2022-01-21 19:16:49 +0600

    stores image using FileSystem

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,5 +1,21 @@
 export const ADD_NEW_PLACE = "ADD_NEW_PLACE";
+import * as FileSystem from "expo-file-system";
 
 export const addNewPlace = (title, image) => {
-  return { type: ADD_NEW_PLACE, placeData: { title, image } };
+  return async (dispatch) => {
+    const fileName = image.split("/").pop();
+    const newPath = FileSystem.documentDirectory + fileName;
+
+    try {
+      await FileSystem.moveAsync({
+        from: image,
+        to: newPath,
+      });
+    } catch (err) {
+      console.error(err);
+      throw new Error(err.message);
+    }
+
+    dispatch({ type: ADD_NEW_PLACE, placeData: { title, image: newPath } });
+  };
 };
